97710794679b95694626c41978a40716fa07eea4
40172327 <40172327@live.napier.ac.uk>
2017-10-05 09:45:50 +0000

    Adding routes to be able to display the list and upload form

--- munroes.py
+++ munroes.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def welcome():
+def home():
   return render_template('home.html')
+
+@app.route("/add-munroe.html/")
+def addm():
+  return render_template('add-munroe.html')
+
+@app.route("/munroe-list.html/")
+def mlist():
+  return render_template('munroe-list.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
