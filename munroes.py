04f8518fef38ed67a67b2601cd80994728185d73
40172327 <40172327@live.napier.ac.uk>
2017-10-05 08:24:51 +0000

    Added structural pages that will be use to display informations later

--- munroes.py
+++ munroes.py
@@ -1,1 +1,9 @@
-list of munroes goes here eventually
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+  return render_template('home.html')
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0')
